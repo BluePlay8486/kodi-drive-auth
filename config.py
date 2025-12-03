82504ec5677592b5822adf4e2a799e814d2240c0
BluePlay <denisbarbosadossantosdbds5545@gmail.com>
2025-12-03 20:31:20 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    BASE_URL = os.getenv("BASE_URL", "https://seu-render.onrender.com")
+    BASE_URL = os.getenv("BASE_URL", "https://kodi-drive-auth.onrender.com")
